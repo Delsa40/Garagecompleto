1a8d4e9de258312e607d8e71b48f1d06441dd821
Delsa40 <nicolasdelsanto4@gmail.com>
2022-08-11 20:46:21 -0300

    Agrego scripts y juego

---
+++ scripts.js
@@ -0,0 +1,15 @@
+// LLAMO FUNCIONES
+
+document.getElementsByClassName("siguienteFoto").addEventListener("click", siguiente);
+
+// DECLARO VARIABLES
+
+var segunda_foto = document.getElementById("segunda");
+
+// FUNCIONES
+
+function siguiente() {
+
+    segunda_foto.style.left = "100px";
+}
+
