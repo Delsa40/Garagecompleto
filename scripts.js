d34b74e3574e47165e6c0befddc348cd17d18990
Delsa40 <nicolasdelsanto4@gmail.com>
2022-08-18 20:06:08 -0300

    Agrego hover a la imagen del modelo y algunos scripts

--- scripts.js
+++ scripts.js
@@ -1,15 +1,51 @@
-// LLAMO FUNCIONES
-
-document.getElementsByClassName("siguienteFoto").addEventListener("click", siguiente);
-
+//-----------------------------------PARA CADA MODELO:
 // DECLARO VARIABLES
 
-var segunda_foto = document.getElementById("segunda");
+var segunda_foto = document.getElementsByClassName("segunda");
+var contador = 0;
+var siguiente_foto = document.getElementById("siguienteFoto");
 
 // FUNCIONES
 
 function siguiente() {
 
-    segunda_foto.style.left = "100px";
+    if(contador==0){
+
+        segunda_foto.classList.add("clickeado");
+        contador=1;
+    }
+    else{
+        segunda_foto.classList.remove("clickeado");
+        contador=0;
+    }
 }
 
+// LLAMO FUNCIONES
+
+siguiente_foto.addEventListener("click", siguiente);
+
+//-----------------------------------PARA LOS JUEGOS:
+//DECLARO VARIABLES
+
+var boton = document.getElementsByClassName("botonOpcion");
+var aux = 0;
+var lista_de_resultados = document.getElementsByClassName("resultados");
+
+//FUNCIONES
+
+function mostrar() {
+
+    if(aux==0){
+
+        lista_de_resultados.classList.add("respuesta");
+        aux = 1;
+    }
+    else{
+        lista_de_resultados.classList.remove("respuesta");
+        aux = 0;
+    }
+}
+
+//LLAMO FUNCIONES
+
+boton.addEventListener("click", mostrar);
