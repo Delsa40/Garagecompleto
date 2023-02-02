c02ef88be1e7e7ff524c57255b2aa4334503b262
Nicolas Del Santo <nicolasdelsanto4>
2023-02-02 12:13:09 -0300

    Agrego títulos a las noticias

--- politicasdeprivacidad.php
+++ politicasdeprivacidad.php
@@ -1,6 +1,6 @@
 <?php require ("header.php") ?>
     <section>
-        <h1>POLÍTICA DE PRIVACIDAD</h1>
+        <h1 class="nombreMarca">POLÍTICA DE PRIVACIDAD</h1>
         <p>
 La presente Política de Privacidad establece los términos en que Garagecompleto usa y protege la información que es proporcionada por sus usuarios al momento de utilizar su sitio web. Esta compañía está comprometida con la seguridad de los datos de sus usuarios. Si en algún momento le pedimos llenar campos de información personal con la cual usted pueda ser identificado, lo hacemos asegurando que sólo se empleará de acuerdo con los términos de este documento. Sin embargo esta Política de Privacidad puede cambiar con el tiempo o ser actualizada por lo que le recomendamos y enfatizamos revisar continuamente esta página para asegurarse que está de acuerdo con dichos cambios.</p>
 <br><br>
