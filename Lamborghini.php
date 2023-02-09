0d95f15ffb50db327048bebc5cf8b45cfea77fd1
Nicolas Del Santo <nicolasdelsanto4>
2023-02-09 11:54:01 -0300

    Agrego la Urus

--- Lamborghini.php
+++ Lamborghini.php
@@ -5,5 +5,9 @@
             <img src="img/HuracanSTO2021.jpg">
             <p><a href="HuracanSTO2021.php">Huracan STO 2021</a></p>
         </article>
+        <article class="modelo">
+            <img src="img/HuracanSTO2021.jpg">
+            <p><a href="Urus.php">Urus</a></p>
+        </article>
     </section>
     <?php require('footer.php') ?>
